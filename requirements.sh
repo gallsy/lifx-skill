2847cd5753f7c4967fbf0dd8c7b62946ac1f7e63
gallsy <joshgall@live.com>
2018-02-19 21:46:42 +1100

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,5 +1,5 @@
 #This file is for any packages you need installed for your skill to run
 cd /opt/mycroft/skills/lifx-skill/
 git clone https://github.com/sharph/lifx-python.git
-cd /lifx-python/
+cd /opt/mycroft/skills/lifx-skill/lifx-python/
 python setup.py install
